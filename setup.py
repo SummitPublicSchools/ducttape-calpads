490b6a42eebd2247ec3964a5bd55a47fc7539d21
Yusuph Mkangara <yusuph.mka@outlook.com>
2019-12-15 21:23:19 -0800

    Bump to version 0.2.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("LICENSE", "r") as li:
 
 setup(
     name="ducttape-calpads",
-    version="0.1.0",
+    version="0.2.0",
     author="Yusuph Mkangara",
     author_email="ymkangara@summitps.org",
     description="Extension of the duct-tape package to automate CALPADS data extraction",
