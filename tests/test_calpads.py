bb3393089151745022906a6cbd1ef8721ccfbdc3
Yusuph Mkangara <yusuph.mka@outlook.com>
2020-03-28 22:22:57 -0700

    Better the tests

--- tests/test_calpads.py
+++ tests/test_calpads.py
@@ -27,6 +27,23 @@ EXTRACTS = ['ssid', 'crsc', 'crse', 'sass', 'stas', 'scte',
             'sped', 'sela', 'sinf', 'ssrv', 'directcertification']
 YEAR_ONLY_EXTRACTS = ['crsc', 'crse', 'sass', 'scsc', 'stas',
                     'scte', 'scse', 'sdis']
+ODS_REPORTS = ['1.1', '1.2', '1.3', '1.5', '1.9', '1.10', '1.17', '1.18', 
+                '1.19', '1.20', '2.2', '2.8', '5.6', '5.7', '5.8', '5.9',
+                 '8.1', '8.1a', '8.1b', '8.2', '9.1', '9.2', '10.1', '11.1',
+                 '12.1', '2.4', '2.5', '2.6', '2.7', '2.11', '3.1', '3.2', 
+                 '3.3', '3.6', '3.7', '3.8', '4.1', '4.3', '4.4', '3.9', 
+                 '3.10', '3.11', '3.12', '3.13', '3.14', '3.15', '5.1', 
+                 '5.2', '5.3', '5.4', '5.5']
+SNAPSHOT_REPORTS = ['1.0', '1.0a', '1.1', '1.2', '1.3', '1.4', '1.5',
+                    '1.6', '1.7', '1.8', '1.9', '1.10', '1.11', '1.12', 
+                    '1.13', '1.14', '1.17', '1.18', '2.1', '2.2', '2.8',
+                    '2.9', '2.10', '2.12', '2.13', '8.1', '8.1a', '8.1b',
+                    '8.1c', '16.1', '16.2', '16.3', '16.5', '16.6', '2.4',
+                    '2.5', '2.6', '2.7', '2.11', '2.14', '3.1', '3.2', '3.3',
+                    '3.6', '3.7', '3.8', '4.1', '4.2', '4.3', '4.4', '4.5',
+                    '17.1', '17.2', '3.9', '3.10', '3.11', '3.14', '3.15',
+                    '3.16', '3.17', '3.18', '5.1', '5.2', '5.3', '1.21',
+                    '1.21', '5.4', '5.5', '8.1eoy3', '14.1', '14.2']
 
 class TestCalpadsDataSource(unittest.TestCase):
     @classmethod
@@ -47,8 +64,11 @@ class TestCalpadsDataSource(unittest.TestCase):
     def tearDown(self):
         if os.path.exists(config['Calpads']['temp_folder_path']):
             shutil.rmtree(config['Calpads']['temp_folder_path'])
-        if self.cp.driver.service.is_connectable():
-            self.cp.driver.quit()
+        try:
+            if self.cp.driver.service.is_connectable():
+                self.cp.driver.quit()
+        except AttributeError:
+            pass
 
     def test_download_url_report(self):
         with self.assertRaises(NotImplementedError):
@@ -150,11 +170,28 @@ class TestCalpadsReports(unittest.TestCase):
             self.cp.driver.quit()
     
     def test_download_snapshot_report(self):
-        self.assertTrue(isinstance(self.cp.download_snapshot_report(lea_code=config['Calpads']['test_lea'],
-                                                                report_code='1.2', dry_run=False), 
-                                    pd.DataFrame))
+        for snapshot in SNAPSHOT_REPORTS:
+            with self.subTest(snapshot=snapshot):
+                try:
+                    self.assertTrue(isinstance(self.cp.download_snapshot_report(lea_code=config['Calpads']['test_lea'],
+                                                                        report_code=snapshot, 
+                                                                        dry_run=False), 
+                                            pd.DataFrame))
+                except:
+                    #Need to make sure the driver still closes even if it fails for some reason -- wasn't always happening
+                    if self.cp.driver.service.is_connectable():
+                        self.cp.driver.quit()
+                    raise
     
     def test_download_ods_report(self):
-        self.assertTrue(isinstance(self.cp.download_ods_report(lea_code=config['Calpads']['test_lea'],
-                                                                report_code='1.2', dry_run=False), 
-                                    pd.DataFrame))
+        for ods in ODS_REPORTS:
+            with self.subTest(ods=ods):
+                try:
+                    self.assertTrue(isinstance(self.cp.download_ods_report(lea_code=config['Calpads']['test_lea'],
+                                                                            report_code=ods, 
+                                                                            dry_run=False), 
+                                                pd.DataFrame))
+                except:
+                    if self.cp.driver.service.is_connectable():
+                        self.cp.driver.quit()
+                    raise
